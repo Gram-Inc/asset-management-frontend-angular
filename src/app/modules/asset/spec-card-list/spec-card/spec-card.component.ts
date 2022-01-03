12726217e4cc5bfca5b1c33b6dba324a4c85c8c9
= <=>
2022-01-03 12:46:17 +0530

    Chk Pint

--- src/app/modules/asset/spec-card-list/spec-card/spec-card.component.ts
+++ src/app/modules/asset/spec-card-list/spec-card/spec-card.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-spec-card',
-  templateUrl: './spec-card.component.html',
-  styleUrls: ['./spec-card.component.scss']
+  selector: "app-spec-card",
+  templateUrl: "./spec-card.component.html",
+  styleUrls: ["./spec-card.component.scss"],
 })
 export class SpecCardComponent implements OnInit {
+  type: "HDD" | "SSD" | "NVMe" | "RAM" = "SSD";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
