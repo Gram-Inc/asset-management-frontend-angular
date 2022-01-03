01261313da1364e86e4809da54ec1dcadf9271da
= <=>
2022-01-03 20:02:54 +0530

    ChkPoint

--- src/app/modules/uam/uam-detail/uam-detail.component.ts
+++ src/app/modules/uam/uam-detail/uam-detail.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-uam-detail',
-  templateUrl: './uam-detail.component.html',
-  styleUrls: ['./uam-detail.component.scss']
+  selector: "app-uam-detail",
+  templateUrl: "./uam-detail.component.html",
+  styleUrls: ["./uam-detail.component.scss"],
 })
 export class UamDetailComponent implements OnInit {
+  requestType: "c" | "m" | "d" | "de" = "m";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
