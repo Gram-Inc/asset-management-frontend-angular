88c229cfeef7a86dc934237fff832063515c826b
= <=>
2022-01-05 18:02:42 +0530

    Dashboard Type Changed

--- src/app/core/dashboard/dashboard.types.ts
+++ src/app/core/dashboard/dashboard.types.ts
@@ -1,21 +1,59 @@
 export interface IDashboard {
-  assetStatusCount: {
-    IN_POOL: number;
-    ASSIGNED: number;
-    SCRAP: number;
-    DOWN: number;
+  assets?: IAssetDashboard;
+  ticket?: ITicketDashboard;
+  users?: IUserDashboard;
+  itUserSpecificDetails?: IITUserSpecificDetailDashboard;
+}
+interface IAssetDashboard {
+  assetStatusCount?: {
+    IN_POOL?: number;
+    ASSIGNED?: number;
+    SCRAP?: number;
+    DOWN?: number;
   };
-  ticketStatusCount: {
-    Assigned: number;
-    Closed: number;
-    Hold: number;
-    "In Progress": number;
-    Open: number;
-    "Vendor Dependency": number;
+  assetsCountWithUpcommingExpireingInDays?: {
+    WARRANTY?: number;
+    AMC?: number;
   };
-  assetsCountWithUpcommingExpireingInDays: {
-    WARRANTY: number;
-    AMC: number;
+  registeredAssets?: number;
+  lastTenCreatedAssetsDetails?: {
+    _id?: string;
+    serialNo?: string;
+    hostName?: string;
+    type?: string;
+    createdAt?: string;
+  }[];
+  branchWiseOverallAssets?: { _id?: string; count?: number; branchCode?: string; name?: string }[];
+}
+interface ITicketDashboard {
+  ticketStatusCount?: {
+    Assigned?: number;
+    Closed?: number;
+    Hold?: number;
+    "In Progress"?: number;
+    Open?: number;
+    "Vendor Dependency"?: number;
   };
-  registeredAssets: number;
+}
+interface IITUserSpecificDetailDashboard {
+  assignedTickets?: number;
+  todaysCompletedTickets?: number;
+  ongoingTickets?: number;
+}
+interface IUserDashboard {
+  totalUsers?: number;
+  thisWeekNewJoinees?: {
+    _id?: string;
+    firstName?: string;
+    lastName?: string;
+    isAssetAllocated?: boolean;
+    totalAssetsAllocated?: number;
+    createdAt?: string;
+  }[];
+  branchWiseUsers?: {
+    _id?: string;
+    count?: number;
+    branchCode?: string;
+    name?: string;
+  }[];
 }
