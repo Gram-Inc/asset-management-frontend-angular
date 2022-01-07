a1e64d28b66c5004958617cfa3e38fe0c8c4d0ae
= <=>
2022-01-07 11:26:20 +0530

    user detail page Design ready

--- src/app/modules/user/user-full-detail/user-full-detail.module.ts
+++ src/app/modules/user/user-full-detail/user-full-detail.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { UserFullDetailComponent } from "./user-full-detail.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [UserFullDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild([{ path: "", component: UserFullDetailComponent }])],
 })
 export class UserFullDetailModule {}
