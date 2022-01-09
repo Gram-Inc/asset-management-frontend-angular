7129a05756036f5963b208b91d7d58fe7261bd72
= <=>
2022-01-09 18:12:22 +0530

    CPU speccard

--- src/app/modules/asset/spec-card-list/spec-card-list.component.ts
+++ src/app/modules/asset/spec-card-list/spec-card-list.component.ts
@@ -57,4 +57,12 @@ export class SpecCardListComponent implements OnInit {
   ngOnInit(): void {
     //check the type of asset
   }
+
+  getProcessorShortName(value: string) {
+    if (value.includes("i3")) return "i3";
+    else if (value.includes("i5")) return "i5";
+    else if (value.includes("i7")) return "i7";
+    else if (value.includes("i9")) return "i9";
+    else return "N/A";
+  }
 }
