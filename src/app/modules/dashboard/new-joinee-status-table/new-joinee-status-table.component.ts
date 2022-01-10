2ff772a7d041560fb975e3f8d28753ddccd43801
= <=>
2022-01-10 14:39:21 +0530

    Chart Dashbaord

--- src/app/modules/dashboard/new-joinee-status-table/new-joinee-status-table.component.ts
+++ src/app/modules/dashboard/new-joinee-status-table/new-joinee-status-table.component.ts
@@ -9,66 +9,7 @@ import { MatTableDataSource } from "@angular/material/table";
   styleUrls: ["./new-joinee-status-table.component.scss"],
 })
 export class NewJoineeStatusTableComponent implements OnInit {
-  @Input() data: IUserDashboard = {
-    thisWeekNewJoinees: [
-      {
-        _id: "61c1496ffc32960c81b7df4a",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: true,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-02T03:26:39.773Z",
-      },
-      {
-        _id: "61d46ce9ab224d842d975864",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: false,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-04T15:51:05.430Z",
-      },
-      {
-        _id: "61d46d158142822a73b22fa0",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: false,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-04T15:51:49.764Z",
-      },
-      {
-        _id: "61d46d3b42d1719864316f54",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: false,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-04T15:52:27.722Z",
-      },
-      {
-        _id: "61d46d3b42d1719864316f54",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: false,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-04T15:52:27.722Z",
-      },
-      {
-        _id: "61d46d3b42d1719864316f54",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: false,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-04T15:52:27.722Z",
-      },
-      {
-        _id: "61d46d3b42d1719864316f54",
-        firstName: "Govind",
-        lastName: "Solanki",
-        isAssetAllocated: false,
-        totalAssetsAllocated: 0,
-        createdAt: "2022-01-04T15:52:27.722Z",
-      },
-    ],
-  };
+  @Input() data: IUserDashboard;
   @ViewChild("recentTransactionsTable", { read: MatSort }) recentTransactionsTableMatSort: MatSort;
   recentTransactionsDataSource: MatTableDataSource<INewJoineeDashboard> = new MatTableDataSource();
   recentTransactionsTableColumns: string[] = ["date", "name", "status"];
