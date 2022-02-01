ba8abd18e32c88c249c06e611328925f8b254c61
= <=>
2022-02-01 18:18:54 +0530

    UAM Permission Complete

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,98 +1,129 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-  ViewChild,
-} from "@angular/core";
+import
+   {
+      AfterViewInit,
+      ChangeDetectorRef,
+      Component,
+      OnChanges,
+      OnDestroy,
+      OnInit,
+      SimpleChanges,
+      ViewChild,
+   } from "@angular/core";
 import { MatDrawer, MatDrawerMode } from "@angular/material/sidenav";
 import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
-import { Observable, Subject } from "rxjs";
-import { filter, takeUntil } from "rxjs/operators";
+import { Observable, of, Subject } from "rxjs";
+import { filter, switchMap, takeUntil } from "rxjs/operators";
 import { AuthService } from "../core/auth/auth.service";
+import { PermissionService } from "../core/auth/permission.service";
+import { AccessType, ModuleTypes } from "../core/auth/permission.types";
 import { SideNavService } from "../core/side-nav/side-nav.service";
 import { UserService } from "../core/user/user.service";
 import { IUser } from "../core/user/user.types";
 
 @Component({
-  selector: "layout",
-  templateUrl: "./layout.component.html",
-  styleUrls: ["./layout.component.scss"],
+   selector: "layout",
+   templateUrl: "./layout.component.html",
+   styleUrls: ["./layout.component.scss"],
 })
-export class LayoutComponent implements OnInit, OnDestroy, AfterViewInit, OnChanges {
-  user$: Observable<IUser> = undefined;
-  mode: MatDrawerMode = "side";
-  @ViewChild("drawer") public drawer: MatDrawer;
+export class LayoutComponent implements OnInit, OnDestroy, AfterViewInit, OnChanges
+{
+   user$: Observable<IUser> = undefined;
+   mode: MatDrawerMode = "side";
+   @ViewChild("drawer") public drawer: MatDrawer;
 
-  private _unsubscribeAll: Subject<any> = new Subject<any>();
-  constructor(
-    private _userService: UserService,
-    private _changeDetectorRef: ChangeDetectorRef,
-    private _modeService: SideNavService,
-    private _router: Router,
-    private _authService: AuthService
-  ) {}
-  //c
-  ngOnChanges(changes: SimpleChanges): void {
-    if (this._router.isActive("/uam/create", false)) this.drawer.opened = false;
-  }
-  ngOnInit(): void {
-    // Attach a listener to the NavigationEnd event
-    this._router.events
-      .pipe(
-        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
-        takeUntil(this._unsubscribeAll)
-      )
-      .subscribe((event: NavigationEnd) => {
-        // Mark if active
-        if (this._router.isActive("/uam/create", false)) this.drawer.opened = false;
+   private _unsubscribeAll: Subject<any> = new Subject<any>();
+   constructor(
+      private _userService: UserService,
+      private _changeDetectorRef: ChangeDetectorRef,
+      private _modeService: SideNavService,
+      private _router: Router,
+      private _authService: AuthService,
+      private _permissionService: PermissionService
+   ) { }
+   //c
+   ngOnChanges(changes: SimpleChanges): void
+   {
+      if (this._router.isActive("/uam/create", false)) this.drawer.opened = false;
+   }
+   ngOnInit(): void
+   {
+      // Attach a listener to the NavigationEnd event
+      this._router.events
+         .pipe(
+            filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+            takeUntil(this._unsubscribeAll)
+         )
+         .subscribe((event: NavigationEnd) =>
+         {
+            // Mark if active
+            if (this._router.isActive("/uam/create", false)) this.drawer.opened = false;
+         });
+
+      this._modeService.mode$.pipe(takeUntil(this._unsubscribeAll)).subscribe((val) =>
+      {
+         this.mode = val;
       });
 
-    this._modeService.mode$.pipe(takeUntil(this._unsubscribeAll)).subscribe((val) => {
-      this.mode = val;
-    });
+      // this._userService.user$.pipe(takeUntil(this._unsubscribeAll)).subscribe((user: IUser) => {
+      //   this.user = user;
+      // });
+      this.user$ = this._userService.user$;
+   }
+   ngOnDestroy()
+   {
+      this._unsubscribeAll.next();
+      this._unsubscribeAll.complete();
+   }
+   ngAfterViewInit(): void
+   {
+      this._modeService.setDrawer(this.drawer);
+   }
+   toggleDrawer()
+   {
+      this.drawer.toggle();
+   }
 
-    // this._userService.user$.pipe(takeUntil(this._unsubscribeAll)).subscribe((user: IUser) => {
-    //   this.user = user;
-    // });
-    this.user$ = this._userService.user$;
-  }
-  ngOnDestroy() {
-    this._unsubscribeAll.next();
-    this._unsubscribeAll.complete();
-  }
-  ngAfterViewInit(): void {
-    this._modeService.setDrawer(this.drawer);
-  }
-  toggleDrawer() {
-    this.drawer.toggle();
-  }
-
-  checkTicket() {
-    return this._authService.checkTicket();
-  }
-  checkUAM() {
-    return this._authService.checkUAM();
-  }
-  checkAsset() {
-    return this._authService.checkAsset();
-  }
-  checkUser() {
-    return this._authService.checkUser();
-  }
-  checkVendor() {
-    return this._authService.checkVendor();
-  }
-  checkBranch() {
-    return this._authService.checkBranch();
-  }
-  checkReport() {
-    return this._authService.checkReport();
-  }
-  checkSetting() {
-    return this._authService.checkSetting();
-  }
+   checkTicket()
+   {
+      return this._authService.checkTicket();
+   }
+   checkUAM()
+   {
+      return this._authService.checkUAM();
+   }
+   checkAsset()
+   {
+      return this._authService.checkAsset();
+   }
+   checkUser()
+   {
+      return this._authService.checkUser();
+   }
+   checkVendor()
+   {
+      return this._authService.checkVendor();
+   }
+   checkBranch()
+   {
+      return this._authService.checkBranch();
+   }
+   checkReport()
+   {
+      return this._authService.checkReport();
+   }
+   checkSetting()
+   {
+      return this._authService.checkSetting();
+   }
+   canCreateUAM()
+   {
+      return this._permissionService.checkCurrentUserPermission(ModuleTypes.UAM).pipe(
+         switchMap(value =>
+         {
+            //User should be able to Create only if He has Readwrite or full access
+            if (value == AccessType.ReadWrite || value == AccessType.FullAccess) return of(true);
+            return of(false);
+         })
+      );
+   }
 }
--- src/app/modules/uam/uam-list/uam-list.component.ts
+++ src/app/modules/uam/uam-list/uam-list.component.ts
@@ -1,19 +1,22 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-  ViewEncapsulation,
-} from "@angular/core";
+import
+   {
+      AfterViewInit,
+      ChangeDetectorRef,
+      Component,
+      OnDestroy,
+      OnInit,
+      ViewChild,
+      ViewEncapsulation,
+   } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { MatDialog } from "@angular/material/dialog";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
-import { merge, Observable, Subject } from "rxjs";
+import { merge, Observable, of, Subject } from "rxjs";
 import { debounceTime, map, switchMap, takeUntil } from "rxjs/operators";
 import { IPagination } from "src/app/core/asset/asset.types";
+import { PermissionService } from "src/app/core/auth/permission.service";
+import { AccessType, ModuleTypes } from "src/app/core/auth/permission.types";
 import { UamService } from "src/app/core/uam/uam.service";
 import { IUAM, IUserInformationUAM } from "src/app/core/uam/uam.types";
 import { UserService } from "src/app/core/user/user.service";
@@ -21,11 +24,11 @@ import { IUser } from "src/app/core/user/user.types";
 import { RikielConfirmationService } from "src/app/custom/confirmation/confirmation.service";
 
 @Component({
-  selector: "uam-list",
-  templateUrl: "./uam-list.component.html",
-  styles: [
-    /* language=SCSS */
-    `
+   selector: "uam-list",
+   templateUrl: "./uam-list.component.html",
+   styles: [
+      /* language=SCSS */
+      `
       .inventory-grid {
         grid-template-columns: auto 40px;
 
@@ -42,154 +45,188 @@ import { RikielConfirmationService } from "src/app/custom/confirmation/confirmat
         }
       }
     `,
-  ],
-  encapsulation: ViewEncapsulation.None,
+   ],
+   encapsulation: ViewEncapsulation.None,
 })
-export class UamListComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild(MatPaginator) private _paginator: MatPaginator;
-  @ViewChild(MatSort) private _sort: MatSort;
+export class UamListComponent implements OnInit, AfterViewInit, OnDestroy
+{
+   @ViewChild(MatPaginator) private _paginator: MatPaginator;
+   @ViewChild(MatSort) private _sort: MatSort;
 
-  private _unsubscribeAll: Subject<any> = new Subject<any>();
-  uams$: Observable<IUAM[]>;
-  types: string[];
-  pagination: IPagination;
-  selectedUAM: IUAM | null = null;
-  flashMessage: "success" | "error" | null = null;
+   private _unsubscribeAll: Subject<any> = new Subject<any>();
+   uams$: Observable<IUAM[]>;
+   types: string[];
+   pagination: IPagination;
+   selectedUAM: IUAM | null = null;
+   flashMessage: "success" | "error" | null = null;
 
-  selectedAssetForm: FormGroup;
-  isLoading: boolean = false;
-  searchCtrl: FormControl = new FormControl("");
+   selectedAssetForm: FormGroup;
+   isLoading: boolean = false;
+   searchCtrl: FormControl = new FormControl("");
 
-  constructor(
-    private _uamService: UamService,
-    private _formBuilder: FormBuilder,
-    private _changeDetectorRef: ChangeDetectorRef,
-    private _rikielConfirmationService: RikielConfirmationService,
-    private _matDialog: MatDialog
-  ) {}
+   constructor(
+      private _uamService: UamService,
+      private _formBuilder: FormBuilder,
+      private _changeDetectorRef: ChangeDetectorRef,
+      private _rikielConfirmationService: RikielConfirmationService,
+      private _matDialog: MatDialog,
+      private _permissionService: PermissionService
+   ) { }
 
-  ngOnInit(): void {
-    // If Search value changes
-    this.searchCtrl.valueChanges
-      .pipe(
-        takeUntil(this._unsubscribeAll),
-        debounceTime(300),
-        switchMap((query) => {
-          this.isLoading = true;
-          return this._uamService.getUAMS(1, 10, query);
-        }),
-        map(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe();
+   ngOnInit(): void
+   {
+      // If Search value changes
+      this.searchCtrl.valueChanges
+         .pipe(
+            takeUntil(this._unsubscribeAll),
+            debounceTime(300),
+            switchMap((query) =>
+            {
+               this.isLoading = true;
+               return this._uamService.getUAMS(1, 10, query);
+            }),
+            map(() =>
+            {
+               this.isLoading = false;
+            })
+         )
+         .subscribe();
 
-    // Get the Assets
-    this.uams$ = this._uamService.uams$.pipe(takeUntil(this._unsubscribeAll));
+      // Get the Assets
+      this.uams$ = this._uamService.uams$.pipe(takeUntil(this._unsubscribeAll));
 
-    this._uamService.pagination$
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe((paginationResponse: IPagination) => (this.pagination = paginationResponse));
-  }
+      this._uamService.pagination$
+         .pipe(takeUntil(this._unsubscribeAll))
+         .subscribe((paginationResponse: IPagination) => (this.pagination = paginationResponse));
+   }
 
-  ngAfterViewInit(): void {
-    if (this._sort && this._paginator) {
-      // Set the initial sort
-      /* this._sort.sort({
-        id: "name",
-        start: "asc",
-        disableClear: true,
-      }); */
+   ngAfterViewInit(): void
+   {
+      if (this._sort && this._paginator)
+      {
+         // Set the initial sort
+         /* this._sort.sort({
+           id: "name",
+           start: "asc",
+           disableClear: true,
+         }); */
+
+         // Mark for check
+         this._changeDetectorRef.markForCheck();
+
+         // If the user changes the sort order...
+         this._sort.sortChange.pipe(takeUntil(this._unsubscribeAll)).subscribe(() =>
+         {
+            // Reset back to the first page
+            this._paginator.pageIndex = 0;
+         });
+
+         // Get products if sort or page changes
+         merge(this._sort.sortChange, this._paginator.page)
+            .pipe(
+               switchMap(() =>
+               {
+                  this.isLoading = true;
+                  return this._uamService.getUAMS(
+                     this._paginator.pageIndex,
+                     this._paginator.pageSize
+                     /* "",
+                     "",
+                     "",
+                     this._sort.direction,
+                     this._sort.active */
+                  );
+               }),
+               map(() =>
+               {
+                  this.isLoading = false;
+               })
+            )
+            .subscribe();
+      }
+   }
+   /**
+    * On destroy
+    */
+   ngOnDestroy(): void
+   {
+      // Unsubscribe from all subscriptions
+      this._unsubscribeAll.next();
+      this._unsubscribeAll.complete();
+   }
+
+   /**
+    * Show flash message
+    */
+   showFlashMessage(type: "success" | "error"): void
+   {
+      // Show the message
+      this.flashMessage = type;
 
       // Mark for check
       this._changeDetectorRef.markForCheck();
 
-      // If the user changes the sort order...
-      this._sort.sortChange.pipe(takeUntil(this._unsubscribeAll)).subscribe(() => {
-        // Reset back to the first page
-        this._paginator.pageIndex = 0;
-      });
+      // Hide it after 3 seconds
+      setTimeout(() =>
+      {
+         this.flashMessage = null;
 
-      // Get products if sort or page changes
-      merge(this._sort.sortChange, this._paginator.page)
-        .pipe(
-          switchMap(() => {
-            this.isLoading = true;
-            return this._uamService.getUAMS(
-              this._paginator.pageIndex,
-              this._paginator.pageSize
-              /* "",
-              "",
-              "",
-              this._sort.direction,
-              this._sort.active */
-            );
-          }),
-          map(() => {
-            this.isLoading = false;
-          })
-        )
-        .subscribe();
-    }
-  }
-  /**
-   * On destroy
-   */
-  ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next();
-    this._unsubscribeAll.complete();
-  }
+         // Mark for check
+         this._changeDetectorRef.markForCheck();
+      }, 3000);
+   }
 
-  /**
-   * Show flash message
-   */
-  showFlashMessage(type: "success" | "error"): void {
-    // Show the message
-    this.flashMessage = type;
+   /**
+    * Track by function for ngFor loops
+    *
+    * @param index
+    * @param item
+    */
+   trackByFn(index: number, item: any): any
+   {
+      return item._id || index;
+   }
 
-    // Mark for check
-    this._changeDetectorRef.markForCheck();
+   // Check For Branch Code
+   getBranchShortCode(obj: any)
+   {
+      if (obj) return typeof obj === "object" ? obj.branchCode : "-";
+      return "NULL";
+   }
 
-    // Hide it after 3 seconds
-    setTimeout(() => {
-      this.flashMessage = null;
+   displayFnCreatedBy(createdBy?: string | IUser)
+   {
+      if (createdBy && typeof createdBy == "object") return createdBy.firstName + " " + createdBy.lastName;
+      return "-";
+   }
+   displayFnNoOfUser(userInformation: IUserInformationUAM)
+   {
+      if (!userInformation) return "";
+      if (userInformation.users && userInformation.users.length > 1)
+         return (
+            userInformation.users[0].firstName +
+            " " +
+            userInformation.users[0].lastName +
+            " + " +
+            userInformation.users.length.toString()
+         );
+      return userInformation.users[0].firstName + " " + userInformation.users[0].lastName;
+   }
 
-      // Mark for check
-      this._changeDetectorRef.markForCheck();
-    }, 3000);
-  }
+   /**
+*
+* Font View Manipulators ------Permissions
+*/
 
-  /**
-   * Track by function for ngFor loops
-   *
-   * @param index
-   * @param item
-   */
-  trackByFn(index: number, item: any): any {
-    return item._id || index;
-  }
-
-  // Check For Branch Code
-  getBranchShortCode(obj: any) {
-    if (obj) return typeof obj === "object" ? obj.branchCode : "-";
-    return "NULL";
-  }
-
-  displayFnCreatedBy(createdBy?: string | IUser) {
-    if (createdBy && typeof createdBy == "object") return createdBy.firstName + " " + createdBy.lastName;
-    return "-";
-  }
-  displayFnNoOfUser(userInformation: IUserInformationUAM) {
-    if (!userInformation) return "";
-    if (userInformation.users && userInformation.users.length > 1)
-      return (
-        userInformation.users[0].firstName +
-        " " +
-        userInformation.users[0].lastName +
-        " + " +
-        userInformation.users.length.toString()
+   canEdit(): Observable<boolean>
+   {
+      return this._permissionService.checkCurrentUserPermission(ModuleTypes.UAM).pipe(
+         switchMap(value =>
+         {
+            //User should be able to Create only if He has Readwrite or full access
+            if (value == AccessType.ReadWrite || value == AccessType.FullAccess) return of(true);
+            return of(false);
+         })
       );
-    return userInformation.users[0].firstName + " " + userInformation.users[0].lastName;
-  }
+   }
 }
