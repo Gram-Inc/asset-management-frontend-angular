d8fca22dd65a3d922cb871ec0c5add8d3d1aecaa
Govindram Solanki <gram01846@gmail.com>
2022-10-11 15:56:18 +0530

    QR Issue Resolved

--- src/app/modules/dashboard/total-user-card/total-user-card.component.ts
+++ src/app/modules/dashboard/total-user-card/total-user-card.component.ts
@@ -3,82 +3,87 @@ import { ApexOptions } from "ng-apexcharts";
 import { IUserDashboard } from "src/app/core/dashboard/dashboard.types";
 
 @Component({
-  selector: "total-user-card",
-  templateUrl: "./total-user-card.component.html",
+   selector: "total-user-card",
+   templateUrl: "./total-user-card.component.html",
 })
-export class TotalUserCardComponent implements OnInit {
-  @Input() data: IUserDashboard;
-  chartUserDistribution: ApexOptions = {};
+export class TotalUserCardComponent implements OnInit
+{
+   @Input() data: IUserDashboard;
+   chartUserDistribution: ApexOptions = {};
 
-  constructor() {}
+   constructor() { }
 
-  ngOnInit(): void {
-    this._prepareChartData();
-  }
+   ngOnInit(): void
+   {
+      this._prepareChartData();
+   }
 
-  /**
-   * Prepare the chart data from the data
-   *
-   * @private
-   */
-  private _prepareChartData(): void {
-    this.chartUserDistribution = {
-      chart: {
-        fontFamily: "inherit",
-        foreColor: "inherit",
-        height: "100%",
-        type: "polarArea",
-        toolbar: {
-          show: false,
-        },
-        zoom: {
-          enabled: false,
-        },
-      },
-      labels: this.data.branchWiseUsers.map((x) => {
-        return x.branchCode;
-      }),
-      legend: {
-        position: "bottom",
-      },
-      plotOptions: {
-        polarArea: {
-          spokes: {
-            connectorColors: "#e2e8f0",
-          },
-          rings: {
-            strokeColor: "#e2e8f0",
-          },
-        },
-      },
-      series: this.data.branchWiseUsers.map((x) => {
-        return x.count;
-      }),
-      states: {
-        hover: {
-          filter: {
-            type: "darken",
-            value: 0.75,
-          },
-        },
-      },
-      stroke: {
-        width: 2,
-      },
-      theme: {
-        palette: "palette1",
-      },
-      tooltip: {
-        followCursor: true,
-        theme: "dark",
-      },
-      yaxis: {
-        labels: {
-          style: {
-            colors: "#334155",
-          },
-        },
-      },
-    };
-  }
+   /**
+    * Prepare the chart data from the data
+    *
+    * @private
+    */
+   private _prepareChartData(): void
+   {
+      this.chartUserDistribution = {
+         chart: {
+            fontFamily: "inherit",
+            foreColor: "inherit",
+            height: "100%",
+            type: "polarArea",
+            toolbar: {
+               show: false,
+            },
+            zoom: {
+               enabled: false,
+            },
+         },
+         labels: this.data.branchWiseUsers.map((x) =>
+         {
+            return x.branchCode;
+         }),
+         legend: {
+            position: "bottom",
+         },
+         plotOptions: {
+            polarArea: {
+               spokes: {
+                  connectorColors: "#e2e8f0",
+               },
+               rings: {
+                  strokeColor: "#e2e8f0",
+               },
+            },
+         },
+         series: this.data.branchWiseUsers.map((x) =>
+         {
+            return x.count;
+         }),
+         states: {
+            hover: {
+               filter: {
+                  type: "darken",
+                  value: 0.75,
+               },
+            },
+         },
+         stroke: {
+            width: 2,
+         },
+         theme: {
+            palette: "palette1",
+         },
+         tooltip: {
+            followCursor: true,
+            theme: "dark",
+         },
+         yaxis: {
+            labels: {
+               style: {
+                  colors: "#334155",
+               },
+            },
+         },
+      };
+   }
 }
